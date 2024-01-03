72f0db6a8f4cd57462a9127aab7f6b542ac88f69
chudaa <155516406+chudaa@users.noreply.github.com>
2024-01-03 12:02:36 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAiBM3Vtb0dEVXj6CehitWFrgJ44z_yC_M",
+  authDomain: "choden-1fd82.firebaseapp.com",
+  projectId: "choden-1fd82",
+  storageBucket: "choden-1fd82.appspot.com",
+  messagingSenderId: "791978690709",
+  appId: "1:791978690709:web:4d96ce37196373def6b39c",
+  measurementId: "G-1ZQV79891L"
+};
